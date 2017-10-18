e20f9bc2dccd2afbb0c8c0c911dde69ee130cde5
aurelben <aurel.pigot@gmail.com>
2017-10-18 23:41:24 +0200

    add mycalendar component

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,56 +1,98 @@
-import React from 'react';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import * as styles from '../style';
-import { connect } from 'react-redux';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-import { Header, Text } from 'react-native-elements';
 import * as actions from '../actions/actionsCreators';
 
-class MyCalendar extends React.Component {
+import React, { Component } from 'react'
+import {View, StyleSheet,Text} from 'react-native'
 
+import { Agenda } from 'react-native-calendars';
+
+export default class MyCalendar extends Component {
+    
     render() {
-        // Prop is not loaded yet
-        if (this.props.home === undefined) {
-            return (
-                <View>
-                    <Header
-                    statusBarProps={{ barStyle: 'light-content' }}
-                    outerContainerStyles={{ backgroundColor: '#3D6DCC' }}
-                    centerComponent={{ text: 'Calendar', style: { color: '#fff' } }}
-                    />
-                    <ActivityIndicator size='large' style={{marginTop:130}} />
-                </View>
-            );
-        }
-        // User has a home
-        if (this.props.home) {
-            return (
-                <View>
-                    <Header
-                    statusBarProps={{ barStyle: 'light-content' }}
-                    outerContainerStyles={{ backgroundColor: '#3D6DCC' }}
-                    centerComponent={{ text: 'Calendar', style: { color: '#fff' } }}
-                    />
-                </View>
-            );
-        }
-
-        // User doesn't have a home
+        this.state = {};
+        this.date = new Date().toString()
         return (
-            <View>
-                <Header
-                    statusBarProps={{ barStyle: 'light-content' }}
-                    outerContainerStyles={{ backgroundColor: '#3D6DCC' }}
-                    centerComponent={{ text: 'Calendar', style: { color: '#fff' } }}
-                />
-                <Text h3 style={{marginTop:80}}>PLEASE JOIN A HOME OR CREATE ONE TO USE THE CALENDAR</Text>
-            </View>
+          <Agenda
+            //items={this.state.items}
+            items = {{
+                '2017-10-23': [{text: 'item 1 - any js object', name: "test", }],
+                '2017-11-23': [{text: 'item 2 - any js object'}],
+                '2017-11-24': [],
+                '2017-11-25': [{text: 'item 3 - any js object'},{text: 'any js object'}],
+            }}
+            loadItemsForMonth={this.loadItems.bind(this)}
+            selected={new Date().toString()}
+            renderItem={this.renderItem.bind(this)}
+            renderEmptyDate={this.renderEmptyDate.bind(this)}
+            rowHasChanged={this.rowHasChanged.bind(this)}
+            //markingType={'interactive'}
+            //markedDates={{
+            //  '2017-05-08': [{textColor: '#666'}],
+            //  '2017-05-09': [{textColor: '#666'}],
+            //  '2017-05-14': [{startingDay: true, color: 'blue'}, {endingDay: true, color: 'blue'}],
+            //  '2017-05-21': [{startingDay: true, color: 'blue'}],
+            //  '2017-05-22': [{endingDay: true, color: 'gray'}],
+            //  '2017-05-24': [{startingDay: true, color: 'gray'}],
+            //  '2017-05-25': [{color: 'gray'}],
+            //  '2017-05-26': [{endingDay: true, color: 'gray'}]}}
+            // monthFormat={'yyyy'}
+            // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+            //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+          />
         );
-    }
+      }
+
+    loadItems(day) {
+       setTimeout(() => {
+            this.setState({
+                items: {
+                '2017-05-22': [{text: 'item 1 - any js object', name: "test", }],
+                '2017-05-23': [{text: 'item 2 - any js object'}],
+                '2017-05-24': [],
+                '2017-05-25': [{text: 'item 3 - any js object'},{text: 'any js object'}],
+            }
+            });
+        }, 1000);
+        //console.log(`Load Items for ${day.timestamp}-${day.month}`);
+      }
+
+      renderItem(item) {
+        return (
+          <View style={[styles.item, {height: item.height}]}>
+            <Text>{item.name}</Text>
+            <Text>{item.text}</Text>
+
+          </View>
+        );
+      }
+
+      renderEmptyDate() {
+        return (
+          <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+        );
+      }
+
+      rowHasChanged(r1, r2) {
+        return r1.name !== r2.name;
+      }
+
+      timeToString(time) {
+        const date = new Date(time);
+        return date.toISOString().split('T')[0];
+      }
 }
 
-function mapStateToProps(state) {
-    return {home: state.home}
-}
-
-export default connect(mapStateToProps, actions)(MyCalendar);
+const styles = StyleSheet.create({
+      item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17
+      },
+      emptyDate: {
+        height: 15,
+        flex:1,
+        paddingTop: 30
+      }
+});
